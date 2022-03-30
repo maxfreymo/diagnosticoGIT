02b7c8199799b8e71e5a15a96ab6f7b8f68b9151
maxfreymo <mbfrey@uc.cl>
2022-03-30 14:33:00 -0300

    users functions + little fixs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import tweets
+import users
 
 
 def main():
@@ -8,6 +9,7 @@ def main():
     f = open('data.json')
     # Creamos las variables que queremos encontrar:
     top_tweets = []
+    users_tweets = {}
     top_users = []
     top_days = []
     top_hashtags = []
@@ -17,13 +19,14 @@ def main():
     k = 0
     for line in f:
         data = json.loads(line)
-        # print(data["retweetCount"])
-        print(top_tweets)
+        # print(top_tweets)
         top_tweets = tweets.top_10_tweets(data, top_tweets)
+        users_tweets = users.count_user(data, users_tweets)
         k = k + 1
-        if k == 100:
+        if k == 1000:
             break
 
+    top_users = users.top_ten(users_tweets)
     # Closing file
     f.close()
     print(" --- FIN del programa --- ")
